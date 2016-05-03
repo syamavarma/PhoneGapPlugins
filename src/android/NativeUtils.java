bacfe8202cb0909716488abb1bb625c9e98131d2
syamavarma <syamakutty@gmail.com>
2016-05-03 13:43:44 +0530

    Update NativeUtils.java

--- src/android/NativeUtils.java
+++ src/android/NativeUtils.java
@@ -1,20 +1,4 @@
-/*************************************************************************
-* ADOBE CONFIDENTIAL
-* ___________________
-*
-*  Copyright [2014] Adobe Systems Incorporated
-*  All Rights Reserved.
-*
-* NOTICE:  All information contained herein is, and remains
-* the property of Adobe Systems Incorporated and its suppliers,
-* if any.  The intellectual and technical concepts contained
-* herein are proprietary to Adobe Systems Incorporated and its
-* suppliers and are protected by all applicable intellectual property 
-* laws, including trade secret and copyright laws.
-* Dissemination of this information or reproduction of this material
-* is strictly forbidden unless prior written permission is obtained
-* from Adobe Systems Incorporated.
-**************************************************************************/
+
 package com.syama;
 
 import java.io.File;
